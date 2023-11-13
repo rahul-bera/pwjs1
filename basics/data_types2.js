6aafc0f94f84e040375fd2314f4396b7a10f9f23
rahul-bera <111635703+rahul-bera@users.noreply.github.com>
2023-11-13 06:58:40 +0000

    Ex of data_types2

---
+++ basics/data_types2.js
@@ -0,0 +1,19 @@
+//How to declare Array:?
+const numbers=[1,2,3,4]
+let names=["Rahul",
+    "Dinesh",
+    "Subho"
+    ]
+
+    //How to declare Objects??
+    //Object needs keys and values
+const Object_ex={
+    full_name:"Rahul",
+    age:20,
+    IsloggedIn:true
+}
+
+//How to declare functions??
+const hellofunction=function(){
+    console.log("Hello")
+}
