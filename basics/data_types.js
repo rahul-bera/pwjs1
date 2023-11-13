5b915ac7e4f50a452eec9bc1794ec361fafec1d2
rahul-bera <111635703+rahul-bera@users.noreply.github.com>
2023-11-13 06:44:31 +0000

    example of variables

---
+++ basics/data_types.js
@@ -0,0 +1,8 @@
+let number=20////Ex of number
+const name="Rahul"////Ex of String
+let IsloggedIn=true////Ex of Boolean
+const button=Symbol("id")//Ex of Symbol------->>>
+const newnumber=200.9// //Ex of  number--------
+
+const nodeversion=null;////Ex of null------------
+const longnum=46465468544565365834n;//Ex of BigInt------
